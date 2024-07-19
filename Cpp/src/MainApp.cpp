f305e552421633af5bd284d53b5dbd03b254d53e
siddharth <siddhumyname@gmail.com>
2024-07-19 19:38:08 +0530

    Worked on classes, struct wiht enums

--- Cpp/src/MainApp.cpp
+++ Cpp/src/MainApp.cpp
@@ -1,8 +1,81 @@
 #include "Log.h"
 
+enum Example :unsigned int
+{
+	A = 8, B, C
+};
+
 void returnfunc(int& value)
 {
 	value++;
+}
+
+struct Player
+{
+	int x, y;
+	int moveSpeed;
+
+	void Move(int xa, int ya)
+	{
+		x += xa * moveSpeed;
+		y += ya * moveSpeed;
+	}
+};
+
+class logger
+{
+public: 
+	enum levelEnum
+	{
+		LogLevelInfo = 0, LogLevelWarning, LogLevelError
+	};
+	logger()
+	{
+		std::cout << "Log Created" << std::endl;
+	}
+	~logger()
+	{
+		std::cout << "Log Deleted" << std::endl;
+	}
+
+private:
+	int m_LogLevel;
+public:
+	void SetLogLevel(int level)
+	{
+		m_LogLevel = level;
+	}
+
+	void Info(const char* message)
+	{
+		if (m_LogLevel >= LogLevelInfo)
+			std::cout << "[Info: " << message << "]" << std::endl;
+	}
+	void Warning(const char* message)
+	{
+		if (m_LogLevel >= LogLevelWarning)
+			std::cout << "[Warning: " << message << "]" << std::endl;
+	}
+	void Error(const char* message)
+	{
+		if (m_LogLevel >= LogLevelError)
+			std::cout << "[Error: " << message << "]" << std::endl;
+	}
+
+	static void ErrorWrite(const char* message)
+	{
+		std::cout << "[Error: " << message << "]" << std::endl;
+	}
+};
+
+void function()
+{
+	logger log;
+	log.SetLogLevel(log.LogLevelWarning);
+	log.Warning("Hello!");
+
+	logger::ErrorWrite("Hello!");
+	log.ErrorWrite("Hello!");
 }
 
 int main()
@@ -17,6 +90,16 @@ int main()
 
 	returnfunc(var);
 
+	Player player;
+	player.Move(1, -1);
+
+	std::cout << player.x << std::endl;
+
 	std::cout << var << std::endl;
 	returnValue(10);
+
+	////////////////////////////////////////////////
+
+	function();
+	std::cout << B << std::endl;
 }
